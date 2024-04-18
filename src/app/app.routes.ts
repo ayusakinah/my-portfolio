ec942cfcfb9c9a6a72cfd7032ca6a7204b5d23d6
Ayu Dariah Sakinah <ayudariahs@gmail.com>
2024-04-18 21:49:09 +0700

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/home/about/about.component';
+import { SkillComponent } from './components/home/about/skill/skill.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+    },
+    {
+        path: 'home',
+        title: 'App Home Page',
+        component: HomeComponent
+    },
+    {
+        path: 'about',
+        title: 'App About Page',
+        component: AboutComponent
+    },
+    {
+        path: 'skill',
+        title: 'App Skill Page',
+        component: SkillComponent
+    },
+];
---
+++ src/app/components/home/about/skill/skill.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-skill',
+  standalone: true,
+  imports: [],
+  templateUrl: './skill.component.html',
+  styleUrl: './skill.component.scss'
+})
+export class SkillComponent {
+
+}
